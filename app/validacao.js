cdd36706b5b2b7ff2c6678fc0423c7fcefb46134
Aldry Rocha <aldryrocha@gmail.com>
2022-09-13 22:27:20 -0300

    if game over included

--- app/validacao.js
+++ app/validacao.js
@@ -2,7 +2,19 @@ function verificaChuteIsValid(chute){
     const numero = +chute; //verifica se é numero
 
     if(chuteForInvalido(numero)){
-        elementChute.innerHTML += '<div>Inválido</div>';
+        if (chute.toUpperCase() === "GAME OVER") {
+
+            document.body.innerHTML =
+                `
+                <h2>Game Over!!!</h2>
+                <h3>Pressione o botão para jogar novamente</h3>
+                <button id="jogar-novamente" class="btn-jogar" >Jogar novamente</button>
+                `
+                document.body.style.backgroundColor = "black";
+        } else {
+
+            elementoChute.innerHTML += '<div>Valor Inválido</div>';
+        }
         return;
     }
     if(numBigorSmalThanAllow(numero)){
